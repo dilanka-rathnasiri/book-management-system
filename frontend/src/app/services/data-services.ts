569814ca5757c87a5e1027d0c65bb8c828107f4d
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-12-07 14:39:53 +0530

    add http interceptor

--- frontend/src/app/services/data-services.ts
+++ frontend/src/app/services/data-services.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthServices } from './auth-services';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataServices {
-  constructor(
-    private http: HttpClient,
-    private authServices: AuthServices,
-  ) {}
+  constructor(private http: HttpClient) {}
 
   getData(): Observable<any> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.authServices.getToken()()}`,
-    });
-
-    return this.http.get('http://localhost:8000/books', { headers });
+    return this.http.get('http://localhost:8000/books');
   }
 }
